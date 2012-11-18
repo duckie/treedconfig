3342342acfa48c3cc545bb1dc5744b7069fa8dd5
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-11-18 01:18:53 +0100

    work in progress

--- py/tconf.py
+++ py/tconf.py
@@ -3,17 +3,34 @@
 # Code for python 3.x will be provided later
 
 import argparse
+import ConfigParser
+import os
+
+#
+def getConfigFileName():
+    name = '.tconf'
+    if(os.environ.__contains__('TCONFFILE')):
+        envname = os.environ['TCONFFILE']
+        if (0 < len(envname)):
+            name = envname
+
+    return name
 
 
-class ConfigFile:
-    """Class to read and edit a given config file"""
-    def __init__(self, filename):
-        self.file = filename
+def getExecutionPath():
+    return os.getcwd()
 
-    def __del__(self):
-        print('delete')
+def findLocalConfigFile():
+    confname = getConfigFileName()
+    name = None
+    localpath = getExecutionPath()
+    while('/' is not localpath and None is name):
+        if(os.path.isfile(localpath + '/' + confname)):
+            name = localpath + '/' + confname
 
+        localpath = os.path.dirname(localpath)
 
+    return name
 
 def main():
     parser = argparse.ArgumentParser(description='Tree configuration tool')
@@ -21,9 +38,8 @@ def main():
     parser.add_argument('args', metavar='arguments', nargs='*', help='Command parameters. Type \'tconf help [cmd]\' to get some help.')
     cmdline_args = parser.parse_args()
 
+print(findLocalConfigFile())
 main()
-config = ConfigFile('roger')
-
-
+#config = ConfigFile('roger')
 
 
