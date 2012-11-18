8ea8006f80fb76843d118bb71513bd5875f6b21a
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-11-18 21:43:08 +0100

    Ridiculously simple logger system

--- py/tconf.py
+++ py/tconf.py
@@ -4,9 +4,38 @@
 
 import argparse
 import ConfigParser
-import os
+import os, sys
 
-#
+class Logger:
+    I = 0 # I stands for 'Info'
+    W = 1 # W stands for 'Warning'
+    E = 2 # E stands for 'Error'
+    F = 3 # F stands for 'Fatal'
+    verbosity = False
+
+    def __init__(self, verbosity):
+        self.verbosity = verbosity
+ 
+    def log(self, message, errlevel = 3):
+        if(self.verbosity or self.F == errlevel):
+            prefix = 'tconf'
+            if self.verbosity:
+                if self.I == errlevel:
+                    prefix += ' info'
+                if self.W == errlevel:
+                    prefix += ' warning'
+                if self.E == errlevel:
+                    prefix += ' error'
+                if self.F == errlevel:
+                    prefix += ' fatal'
+
+            prefix += ': '
+            print(prefix + message)
+
+        if self.F == errlevel:
+            sys.exit(1)
+
+
 def getConfigFileName():
     name = '.tconf'
     if(os.environ.__contains__('TCONFFILE')):
@@ -20,11 +49,12 @@ def getConfigFileName():
 def getExecutionPath():
     return os.getcwd()
 
+
 def findLocalConfigFile():
     confname = getConfigFileName()
     name = None
     localpath = getExecutionPath()
-    while('/' is not localpath and None is name):
+    while('/' != localpath and None == name):
         if(os.path.isfile(localpath + '/' + confname)):
             name = localpath + '/' + confname
 
@@ -32,13 +62,24 @@ def findLocalConfigFile():
 
     return name
 
+
 def main():
+    lg = Logger(False)
     parser = argparse.ArgumentParser(description='Tree configuration tool')
     parser.add_argument('cmd', metavar='command', nargs=1, help='The command to execute. Type \'tconf help\' to get a list.')
     parser.add_argument('args', metavar='arguments', nargs='*', help='Command parameters. Type \'tconf help [cmd]\' to get some help.')
+    parser.add_argument('-v', '--verbose', nargs='*', help='Display traces. Useful to understand what happens when it sucks.')
     cmdline_args = parser.parse_args()
+    cmd = cmdline_args.cmd[0]
+    args = cmdline_args.args
+    print(cmd)
+    if 'get' == cmd or 'g' == cmd:
+        if 0 == len(args):
+            lg.log('Hey', lg.I)
+            lg.log('What u doin')
+        #print(get(args[]))
 
-print(findLocalConfigFile())
+
 main()
 #config = ConfigFile('roger')
 
